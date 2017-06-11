6f35634c7bef23d0e852f046438b117e13101cf4
Piterden <efremov.a.denis@gmail.com>
2017-06-11 07:07:48 +0300

    Added SetTableTitle command

---
+++ src/Post/Table/Command/SetTableTitle.php
@@ -0,0 +1,53 @@
+<?php namespace Qooco\PppModule\Post\Table\Command;
+
+use Anomaly\PagesModule\Page\Contract\PageRepositoryInterface;
+use Qooco\PppModule\Post\Table\PostTableBuilder;
+
+/**
+ * Class for set table title.
+ */
+class SetTableTitle
+{
+
+    /**
+     * Post table builder
+     *
+     * @var PostTableBuilder
+     */
+    protected $builder;
+
+    /**
+     * Create new instance of SetTableTitle class
+     *
+     * @param PostTableBuilder $builder The builder
+     */
+    public function __construct(PostTableBuilder $builder)
+    {
+        $this->builder = $builder;
+    }
+
+    /**
+     * Handle the command
+     *
+     * @param PageRepositoryInterface $pages Pages repository
+     */
+    public function handle(PageRepositoryInterface $pages)
+    {
+        if ($parent_id = $this->builder->getRequestValue('filter_parent'))
+        {
+            /* @var PageInterface $page */
+            $page = $pages->find($parent_id);
+
+            while ($page)
+            {
+                $title[] = $page->getTitle();
+                $page    = $page->getParent();
+            }
+
+            $this->builder->setOption(
+                'title',
+                implode(' / ', array_reverse($title))
+            );
+        }
+    }
+}
--- src/Post/Table/PostTableBuilder.php
+++ src/Post/Table/PostTableBuilder.php
@@ -1,9 +1,8 @@
 <?php namespace Qooco\PppModule\Post\Table;
 
-use Anomaly\PagesModule\Page\Contract\PageRepositoryInterface;
-use Anomaly\SelectFieldType\SelectFieldType;
-use Qooco\PppModule\Post\Table\Command\ModifyTableFilters;
 use Qooco\PppModule\Post\Table\Command\ModifyTableColumns;
+use Qooco\PppModule\Post\Table\Command\ModifyTableFilters;
+use Qooco\PppModule\Post\Table\Command\SetTableTitle;
 
 class PostTableBuilder extends \Anomaly\PostsModule\Post\Table\PostTableBuilder
 {
@@ -11,26 +10,10 @@ class PostTableBuilder extends \Anomaly\PostsModule\Post\Table\PostTableBuilder
     /**
      * @param PageRepositoryInterface $pages
      */
-    public function onReady(PageRepositoryInterface $pages)
+    public function onReady()
     {
         $this->dispatch(new ModifyTableFilters($this));
         $this->dispatch(new ModifyTableColumns($this));
-
-        if ($parent_id = $this->getRequestValue('filter_parent'))
-        {
-            $page = $pages->find($parent_id);
-
-            //$this->setOption('title', $page->title.' '.$page->path);
-            $title[] = $page->getTitle();
-            $parent  = $page->getParent();
-
-            while ($parent)
-            {
-                $title[] = $parent->getTitle();
-                $parent  = $parent->getParent();
-            }
-
-            $this->setOption('title', join(' / ', array_reverse($title)));
-        }
+        $this->dispatch(new SetTableTitle($this));
     }
 }
