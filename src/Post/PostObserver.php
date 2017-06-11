c38bc2383fba576fe596def22259547a7c487d8f
Piterden <efremov.a.denis@gmail.com>
2017-06-11 08:55:01 +0300

    Fixed observer

--- src/Post/PostObserver.php
+++ src/Post/PostObserver.php
@@ -18,11 +18,9 @@ class PostObserver extends \Anomaly\PostsModule\Post\PostObserver
      *
      * @param EntryInterface $entry
      */
-    public function saved(EntryInterface $entry)
+    public function saving(EntryInterface $entry)
     {
-        parent::saved($entry);
-
-        $entry->load('parent');
+        parent::saving($entry);
 
         $entry->setPath($entry->getPath());
     }
